a7ca8f67581172a4c9c0b1bf1213523623d8fde9
nin <nikolay.nikiforchuk@gmail.com>
2024-02-15 18:21:29 +0100

    Added arguments to configure task1

--- task1.py
+++ task1.py
@@ -1,8 +1,19 @@
+import argparse
 import os
 
-print("Task 1: Hello World!")
+def main(args):
+    print("Task 1: Hello World!")
+    print("Db name: ", args.db_name)
+    print("Prefix: ", args.prefix)
 
-db_name = os.getenv("DB_NAME")
-prefix = os.getenv("PREFIX")
-print("Db name: ", db_name)
-print("Prefix: ", prefix)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument('--db_name', type=str, default="veriff_events")
+    parser.add_argument('--prefix', type=str, default="test")
+    
+    # Add more parameters as needed
+
+    args, _ = parser.parse_known_args()
+    main(args)
